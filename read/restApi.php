463af7da512f9b94c4b5403eed23c07962613337
zzoszz <mkhotamirais@gmail.com>
2022-10-24 08:37:36 +0700

    lumayan

---
+++ read/restApi.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rest Api</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <p>
+        Dahulu membuat program itu simpel, pilihannya cuma desktop dan web. Hanya ada web designer dan web developer saja, belum ada frontend, backend, fullstack, dev ops. API (application programming interface) menghubungkan satu sama lain. Api adalah sekumpulan fungsi, subroutine, protokol komunikasi, atau kakas/tools, untuk membuat prangkat lunak. interface atau antarmuka adalah bagian yang sama antara dua atau lebih komponen terpisah pada sebuah sistem komputer. Graphical User Interface (GUI). <b>Api</b> adalah antarmuka yang berupa kumpulan fungsi yang dapat dipanggil atau dijalankan oleh program lain. Penerapannya sangat luas: bahasa pemrograman, library framework, sistem operasi, web api/web service. Salah satu jenis web service di dalamnya ada <b>Rest API: representational state transfer</b> dan soap (simple access object protocol).Web api atau web service adalah sebuah sistem perangkat lunak yang dibuat untuk mendukung interoperabilitas/interaksi antar dua aplikasi yang berbeda melalui jaringan. Berikut ini yang akan dipelajari:
+    </p>
+    <ul>
+        <li>Apa itu Rest API</li>
+        <li>JSON (membuat dan mengakses JSON: javascript object notation)</li>
+        <li>Public API (simple complex autentikasi)</li>
+        <li>Membuat Rest server</li>
+    </ul>
+    <p>Adapun prerequest, syarat mempelajari Rest Api pada seri ini adalah:</p>
+    <ul>
+        <li>Js Dasar</li>
+        <li>Object</li>
+        <li>DOM</li>
+        <li>PHP dasar</li>
+        <li>CodeIgniter</li>
+    </ul>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
